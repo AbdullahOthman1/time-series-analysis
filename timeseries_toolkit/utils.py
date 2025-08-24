fdcc8c630da2a65617aed3930ce7456e3db5505d
Abdullah S. Othman <116751179+AbdullahOthman1@users.noreply.github.com>
2025-08-24 22:24:48 +0300

    Delete timeseries_toolkit directory

--- timeseries_toolkit/utils.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-import pandas as pd
-
-def export_csv(obj, path: str | Path):
-    path = Path(path)
-    if isinstance(obj, pd.Series):
-        obj = obj.to_frame()
-    obj.to_csv(path, index=False)
